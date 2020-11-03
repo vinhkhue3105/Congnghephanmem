7a7757ce8fcbdfeb9bb78d79ab5242cc477a3b4a
vinhkhue3105 <71997524+vinhkhue3105@users.noreply.github.com>
2020-11-04 00:03:37 +0700

    Add files via upload

---
+++ Thực hành 5/Thực hành 5/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace Thực_hành_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.CSC = new System.Windows.Forms.TextBox();
+            this.CSM = new System.Windows.Forms.TextBox();
+            this.DNTT = new System.Windows.Forms.TextBox();
+            this.TT = new System.Windows.Forms.TextBox();
+            this.TTCV = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(102, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(318, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tính tiền sinh hoạt bậc thang";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(15, 112);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Chỉ số cũ:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(15, 154);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Chỉ số mới:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(15, 191);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(149, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Điện năng tiêu thụ:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(15, 230);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Thành tiền:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(15, 270);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(166, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Tổng tiền đã có VAT:";
+            // 
+            // CSC
+            // 
+            this.CSC.Location = new System.Drawing.Point(279, 112);
+            this.CSC.Margin = new System.Windows.Forms.Padding(4);
+            this.CSC.Name = "CSC";
+            this.CSC.Size = new System.Drawing.Size(236, 27);
+            this.CSC.TabIndex = 6;
+            // 
+            // CSM
+            // 
+            this.CSM.Location = new System.Drawing.Point(279, 154);
+            this.CSM.Margin = new System.Windows.Forms.Padding(4);
+            this.CSM.Name = "CSM";
+            this.CSM.Size = new System.Drawing.Size(236, 27);
+            this.CSM.TabIndex = 7;
+            // 
+            // DNTT
+            // 
+            this.DNTT.Location = new System.Drawing.Point(279, 191);
+            this.DNTT.Margin = new System.Windows.Forms.Padding(4);
+            this.DNTT.Name = "DNTT";
+            this.DNTT.Size = new System.Drawing.Size(236, 27);
+            this.DNTT.TabIndex = 8;
+            // 
+            // TT
+            // 
+            this.TT.Location = new System.Drawing.Point(279, 230);
+            this.TT.Margin = new System.Windows.Forms.Padding(4);
+            this.TT.Name = "TT";
+            this.TT.Size = new System.Drawing.Size(236, 27);
+            this.TT.TabIndex = 9;
+            // 
+            // TTCV
+            // 
+            this.TTCV.Location = new System.Drawing.Point(279, 270);
+            this.TTCV.Margin = new System.Windows.Forms.Padding(4);
+            this.TTCV.Name = "TTCV";
+            this.TTCV.Size = new System.Drawing.Size(236, 27);
+            this.TTCV.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 321);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 35);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Tính tiền điện";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(202, 321);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 35);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Tiếp tục";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(389, 321);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 35);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Thoát";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 414);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TTCV);
+            this.Controls.Add(this.TT);
+            this.Controls.Add(this.DNTT);
+            this.Controls.Add(this.CSM);
+            this.Controls.Add(this.CSC);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Tính tiền điện";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox CSC;
+        private System.Windows.Forms.TextBox CSM;
+        private System.Windows.Forms.TextBox DNTT;
+        private System.Windows.Forms.TextBox TT;
+        private System.Windows.Forms.TextBox TTCV;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Thực hành 5/Thực hành 5/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Thực_hành_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float x1, x2, x3, x4, x5;
+            x1 = float.Parse(CSC.Text);
+            x2 = float.Parse(CSM.Text);
+            x3 = x2 - x1;
+            DNTT.Text = x3.ToString();
+            if (x3 <= 100 && x3 > 0)
+            {
+                x4 = x3 * 1418;
+                TT.Text = x4.ToString();
+                x5 = x4 * 110 / 100;
+                TTCV.Text = x5.ToString();
+            }
+            else if (x3 <= 150)
+            {
+                x4 = (x3 - 100) * 1622 + 100 * 1418;
+                TT.Text = x4.ToString();
+                x5 = x4 * 110 / 100;
+                TTCV.Text = x5.ToString();
+            }
+            else if (x3 <= 200)
+            {
+                x4 = (x3 - 150) * 2044 + 50 * 1622 + 100 * 1418;
+                TT.Text = x4.ToString();
+                x5 = x4 * 110 / 100;
+                TTCV.Text = x5.ToString();
+            }
+            else if (x3 <= 300)
+            {
+                x4 = (x3 - 200) * 2210 + 50 * 2044 + 50 * 1622 + 100 * 1418;
+                TT.Text = x4.ToString();
+                x5 = x4 * 110 / 100;
+                TTCV.Text = x5.ToString();
+            }
+            else if (x3 <= 400)
+            {
+                x4 = (x3 - 300) * 2361 + 100 * 2210 + 50 * 2044 + 50 * 1622 + 100 * 1418;
+                TT.Text = x4.ToString();
+                x5 = x4 * 110 / 100;
+                TTCV.Text = x5.ToString();
+            }
+            else if (x3 >= 401)
+            {
+                x4 = (x3 - 400) * 2420 + 100 * 2361 + 100 * 2210 + 50 * 2044 + 50 * 1622 + 100 * 1418;
+                TT.Text = x4.ToString();
+                x5 = x4 * 110 / 100;
+                TTCV.Text = x5.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CSC.ResetText();
+            CSM.ResetText();
+            DNTT.ResetText();
+            TT.ResetText();
+            TTCV.ResetText();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
